aed406d2f0093b4e2dc842c3b0419dbd05f9d09b
sw1art <swioneart@gmail.com>
2023-06-16 17:09:23 +0300

    add serch with arg=q

--- django_project/sneakers/urls.py
+++ django_project/sneakers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SneakerListView, SneakerDetailView
+from .views import SneakerListView, SneakerDetailView, SeachResultsListView
 
 urlpatterns = [
     path('', SneakerListView.as_view(), name='sneaker_list'),
     path('<slug:slug>-<uuid:uuid>/', SneakerDetailView.as_view(), name='sneaker_detail'),
+    path('search/', SeachResultsListView.as_view(), name='search_results'),
 ]
 
--- django_project/sneakers/views.py
+++ django_project/sneakers/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
 
 from .models import Sneaker
 
@@ -16,3 +17,13 @@ class SneakerDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView)
     slug_url_kwarg = 'slug'
     login_url = 'account_login'
     permission_required = 'sneakers.base_status'
+
+class SeachResultsListView(ListView):
+    model = Sneaker
+    template_name = 'sneakers/search_results.html'
+    context_object_name = 'sneaker_list'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Sneaker.objects.filter(Q(title__icontains=query))
+
